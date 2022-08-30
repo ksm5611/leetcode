7550be7a39849490ee2f518e68bc60177eab9716
sumin <kim.sumin.wat2014@gmail.com>
2022-08-30 16:09:44 -0700

    two sum and matrix SearchingChallenge

---
+++ matrix.js
@@ -0,0 +1,51 @@
+// how many holes, or contiguous regions of 0's, exist in the matrix.
+
+// Input:["01111",
+//  "01101,
+//  "00011",
+//  "11110"]
+// Output: 3
+
+// Input: ["1011",
+// "0010"]
+// Output: 2
+
+function SearchingChallenge(strArr) {
+  // / code goes here
+  // / replace check hole to
+  const checkHole = (matrix, row, col) => {
+    matrix[row][col] = 2;
+    if (matrix[row][col - 1] === "0") {
+      matrix[row][col - 1] = 2;
+      checkHole(matrix, row, col - 1);
+    }
+    if (matrix[row][col + 1] === "0") {
+      matrix[row][col + 1] = 2;
+      checkHole(matrix, row, col + 1);
+    }
+    if (matrix[row - 1] && matrix[row - 1][col] === "0") {
+      matrix[row - 1][col] = 2;
+      checkHole(matrix, row - 1, col);
+    }
+    if (matrix[row + 1] && matrix[row + 1][col] === "0") {
+      matrix[row + 1][col] = 2;
+      checkHole(matrix, row + 1, col);
+    }
+  };
+
+  const matrix = strArr.map((char) => char.split(""));
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === "0") {
+        checkHole(matrix, i, j);
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+console.log(SearchingChallenge(["01111", "01101", "00011", "11110"]));
+console.log(SearchingChallenge(["1011", "0010"]));
+console.log(SearchingChallenge(["0001", "0001", "0001", "0001"]));
---
+++ twoSum.js
@@ -0,0 +1,39 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+
+var twoSum = function (nums, target) {
+  let empty = [];
+  const maps = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    maps.set(nums[i], i);
+    // console.log(maps.set(nums[i], i));
+  }
+  for (let i = 0; i < nums.length; i++) {
+    console.log("has", maps.has(target - nums[i]));
+    console.log("get", maps.get(target - nums[i]));
+    console.log("push", empty.push(i, maps.get(target - nums[i])));
+    if (maps.has(target - nums[i]) && maps.get(target - nums[i]) !== i) {
+      empty.push(i, maps.get(target - nums[i]));
+      return empty;
+    }
+  }
+  return empty;
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(twoSum([3, 2, 4], 6));
